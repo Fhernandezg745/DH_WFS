f15ab0faae544857e14eeddc2419662140c73d42
Fhernandezg745 <fer.hndez@gmail.com>
2022-04-21 18:07:21 -0300

    module exports

---
+++ superheroes/index.js
@@ -0,0 +1,16 @@
+let superheroes = [
+  {
+    nombre: "batman",
+  },
+  {
+    nombre: "superman",
+  },
+  {
+    nombre: "Iron Man",
+  },
+  {
+    nombre: "La mujer Maravilla",
+  },
+];
+
+module.exports = superheroes;
